4c57158979dae3eabb750161afc794178dd96b9a
Mohamed AbdelraZek <Mohamed AbdelraZek>
2017-10-20 11:20:19 +0200

    Add Image picker to Fetch images from Gallery

--- app/src/main/java/com/mohamedabdelrazek/bloggerapp/BlogActivity.java
+++ app/src/main/java/com/mohamedabdelrazek/bloggerapp/BlogActivity.java
@@ -1,13 +1,42 @@
 package com.mohamedabdelrazek.bloggerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class BlogActivity extends AppCompatActivity {
+    private static final int GALLERY_REQUEST = 11;
+    private ImageButton mImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+        mImageButton = (ImageButton) findViewById(R.id.select_image);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  Toast.makeText(BlogActivity.this, "Sele", Toast.LENGTH_SHORT).show();
+
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+
+            Uri imageUri = data.getData();
+            mImageButton.setImageURI(imageUri);
+
+        }
     }
 }
